f15746b4006117a721cfd0a9348b2b5686bc972f
LukasWuehr <lukas.wuehrggmx.de>
2019-10-21 16:03:19 +0200

    steine setzen und ueberoruefen auf win

--- games/connect4/Connect4Board.java
+++ games/connect4/Connect4Board.java
@@ -4,12 +4,67 @@ import games.*;
  * Connect4Board
  */
 public class Connect4Board extends Board {
-    
+
+    private Disc discs[][];
+    public Connect4Board(int height, int lenght){
+        discs=new Disc[lenght][height];
+    }
    @Override
    public void draw() {
+    for (Disc[] discColum : discs) {        //links oben 0,0
+        for (Disc disc : discColum) {
+            if (disc != null) {
+                System.out.printf("|%d", disc.getColor());
+            } else{
+                System.out.printf("|\t");
+            }
+        }
+        System.out.printf("|\n");
+    }
    }
-   public void updateBoard( ){
 
+   public int setDisc(int coordinate, Player player){
+       int row= discs[coordinate].length-1;
+        while (discs[coordinate][row] != null && row>=-1) {
+            row--;
+        }
+        if (row== -1) { 
+            return 0;
+        } else {
+            discs[coordinate][row]=new Disc(player.getNumber());
+            if (win(coordinate,row)) {
+                return 2;
+            } else {
+                return 1;
+            }
+        }    
+   }
+
+   private boolean win(int lenght, int height){
+       int counterH=0,counterV=0,counterD1=0,counterD2=0;
+       for (int i = -3; i < 4; i++) {
+            if(discs[lenght][height].getColor()==discs[lenght+i][height].getColor()){  // teste horizontale
+                if (++counterH>=4) {
+                    return true;
+                }
+            }
+            if(discs[lenght][height].getColor()==discs[lenght][height+i].getColor()){  // teste Vertikale
+                if (++counterV>=4) {
+                    return true;
+                }
+            }
+            if(discs[lenght][height].getColor()==discs[lenght+i][height+i].getColor()){  // teste Diagonale links oben nach rechts unten
+                if (++counterD1>=4) {
+                    return true;
+                }
+            }
+            if(discs[lenght][height].getColor()==discs[lenght-i][height-i].getColor()){  // teste horizontale links unten nach rechts oben
+                if (++counterD2>=4) {
+                    return true;
+                }
+            }
+        }
+        return false;    
    }
 }
 
