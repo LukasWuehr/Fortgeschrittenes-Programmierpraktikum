6e0d5d423045c93d67a791d7967528df136b3412
LukasWuehr <lukas.wuehrggmx.de>
2019-10-24 13:08:16 +0200

    player wird jetz uebergeben

--- games/connect4/Connect4Game.java
+++ games/connect4/Connect4Game.java
@@ -1,17 +1,18 @@
 package games.connect4;
 import games.*;
 import java.util.Scanner;
+import java.util.Random;
 /**
  * Connect4Game
  */
 public class Connect4Game extends Game {
     private int turns=0;
-    private Player player1= new Player(), player2 = new Player();
+    private Player player1 , player2;
     private Connect4Board board;
-    public Connect4Game(){
-        this.player1 = getPlayer1(); this.player2= getPlayer2();
+    public Connect4Game(Player player1, Player player2){
+        this.player1=player1; this.player2=player2;
     }
-    public void start(){
+     public void start(){
         boolean turnStatus;
         System.out.println("Board Height, Length:"); //boardsize
         Scanner scan = new Scanner(System.in);
@@ -37,8 +38,15 @@ public class Connect4Game extends Game {
         Scanner scan = new Scanner(System.in);
         int discIsSet=0;
         while(0==discIsSet){
-            System.out.printf("Player\n Choose Field");//, player.getPlayerName());
+            if (player.getIsHuman()){
+            System.out.printf("%s\n Choose Field(0-%d): ", player.getPlayerName(),board.getLength());
             discIsSet = board.setDisc(scan.nextInt(), player,this.turns);
+            } else {
+                System.out.printf("Computer:\n");
+                Random rn = new Random();
+                discIsSet = board.setDisc(rn.nextInt(board.getLength()), player, this.turns);
+            }
+            
         }
         board.draw();
         if (discIsSet==2) { 
@@ -49,13 +57,13 @@ public class Connect4Game extends Game {
     }
 
     private void win(){
-        Player winner;
+        Player winner = new Player();
         if (turns%2==0) { //gewinner finden
             winner= player1;
         } else {
             winner = player2;
         }
         winner.addWin();
-        System.out.printf("%s Wins", winner.getPlayerName());
+        System.out.printf("%s Wins\n", winner.getPlayerName());
     }
 }
